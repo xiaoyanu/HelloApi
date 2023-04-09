00b5d4174691ccb8c595f67510da008d1c8044eb
xiaoyanu <61397705+xiaoyanu@users.noreply.github.com>
2023-04-09 11:15:19 +0800

    修复JS中存在的错误

--- css/js.js
+++ css/js.js
@@ -175,10 +175,10 @@ Array.prototype.remove = function (value) {
 function copyText() {
     var text = document.getElementById("text").innerHTML;
     var input = document.getElementById("input");
-    input.value = text;
+    input.value = text.replace("&amp;", "&");
     input.select(); //选中文本
     document.execCommand("copy");
     ks.notice("复制成功", {
-        time: 1500
+        time: 1500,
     });
 }
