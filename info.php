dcef804467ee6f906ce9d7331b563fdc360f4ac6
xiaoyanu <61397705+xiaoyanu@users.noreply.github.com>
2023-06-02 08:55:40 +0800

    增加描述和关键词

--- info.php
+++ info.php
@@ -19,6 +19,8 @@ if ($nr['api'][$num]['now'] != "正常" && $nr['api'][$num]['now'] != "收费") 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo $nr['api'][$num]['name'] . " - " . $nr['api'][$num]['txt'] ?>">
+    <meta name="keywords" content="<?php echo $nr['keywords'] ?>">
     <title><?php echo $nr['api'][$num]['name'] ?> - <?php echo $nr['title'] ?></title>
     <link rel="icon" type="image/ico" href="<?php echo $nr['icon'] ?>">
     <link rel="stylesheet" href="css/info.css">
