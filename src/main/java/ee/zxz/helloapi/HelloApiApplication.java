a2dbbc39470b0005f24293560b0b101adcb1ec76
周星星 <1872786834@qq.com>
2026-02-09 15:18:11 +0800

    新增

---
+++ src/main/java/ee/zxz/helloapi/HelloApiApplication.java
@@ -0,0 +1,13 @@
+package ee.zxz.helloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApiApplication.class, args);
+    }
+
+}
