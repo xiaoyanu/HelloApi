af41567814bb25f6513fa49c7512618c5428e349
周星星 <1872786834@qq.com>
2026-02-09 15:57:26 +0800

    啊

---
+++ server/src/test/java/ee/zxz/helloapi/HelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.zxz.helloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
